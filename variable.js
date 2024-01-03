0aa23fc29511b997c97a42289096c77e8b4d70f5
zubairbinferdous <zubidev71@gmail.com>
2024-01-03 14:18:16 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+// var let const 
+
+var Name = 'zubair';
+var Name = 'joy khan';
+console.log(Name);
+
+let phone = 234239;
+phone = 34343434;
+
+const email = 'abc@gmail.com';
+console.log(phone , email);
